4303b72abedd6ae5a9fb6b7fe2e5327daeb2c329
Samuel Close <10911389@my.uvu.edu>
2020-01-14 23:56:14 -0700

    Class period begin GameartData

---
+++ Game01/Assets/Scripts/ConsoleCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleCheck : MonoBehaviour
+{
+    public string message;
+    void Start()
+    {
+        print(message);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Game01/Assets/Scripts/GameArtData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class GameArtData : ScriptableObject
+{
+    public string playerName;
+    public Sprite sprite;
+    public float playerHealth;
+    public float playerStamina;
+    
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
